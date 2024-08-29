507b34d1eb69bf998d86487042d4acbc80fb387f
aryal31 <113261195+aryal31@users.noreply.github.com>
2024-08-29 13:45:40 +0545

    initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>choose from a diverse menu feature a dectable array of dishes with the finsed blah blah blah blah and more</p>
+        <button>view menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header

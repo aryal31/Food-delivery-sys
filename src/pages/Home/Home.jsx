abe660026c86a72b77086d52668bbe983f1b582a
aryal31 <113261195+aryal31@users.noreply.github.com>
2024-09-03 19:44:00 +0545

    app-download comp added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css"
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
 
 const Home = () => {
 
@@ -13,6 +14,7 @@ const Home = () => {
       <Header/>
       <ExploreMenu category={category} setcategory={setcategory}/>
       <FoodDisplay category={category}></FoodDisplay>
+      <AppDownload></AppDownload>
     </div>
   )
 }

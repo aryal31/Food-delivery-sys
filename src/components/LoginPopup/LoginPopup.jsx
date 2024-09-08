19562037bc3a07a36ce921261f1e336096cc132e
aryal31 <113261195+aryal31@users.noreply.github.com>
2024-09-08 23:28:31 +0545

    still-workigon-login-popup

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginPopup.css'
 
 const LoginPopup = () => {
+
+  const [currState, setCurrState] = useState("Sign Up")
   return (
     <div className="login-popup">
-      
+      <form 
+       className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>
+            {
+              currState
+            }
+          </h2>
+        </div>
+       </form>
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,18 +3,41 @@ import "./Navbar.css";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
-
+const Navbar = ({ setShowLogin }) => {
   const [menu, setmenu] = useState("");
 
   return (
     <div className="navbar">
       <img src={assets.logo} alt="Logo" className="logo" />
       <ul className="navbar-menu">
-        <Link to="/"  onClick={()=>setmenu("home")} className={menu==="home"?"active":""}>home</Link>
-        <a href="#explore-menu"onClick={()=>setmenu("menu")} className={menu==="menu"?"active":""}>menu</a>
-        <a href="#app-download" onClick={()=>setmenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
-        <a href="#footer"onClick={()=>setmenu("contact-us")} className={menu==="contact-us"?"active":""}>contact-us</a>
+        <Link
+          to="/"
+          onClick={() => setmenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setmenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setmenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setmenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact-us
+        </a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
@@ -22,7 +45,13 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
-        <button>sign in</button>
+        <button
+          onClick={() => {
+            setShowLogin(true);
+          }}
+        >
+          sign in
+        </button>
       </div>
     </div>
   );
